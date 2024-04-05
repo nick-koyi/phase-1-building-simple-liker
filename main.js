c8167459e576d5358271b994d78622e629c7ab85
nicholas koyi <koyinicholas@gmail.com>
2024-04-05 17:51:12 +0300

    complete lab

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
-
-
-
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = 'http://mimicServer.example.com', config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
-        reject("Random server error. Try again.");
+        reject('Random server error. Try again.');
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve('Pretend remote server notified of action!');
       }
     }, 300);
+  });
+}
+
+const articleHearts = document.querySelectorAll('.like-glyph');
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener('click', (e) => {
+    const heart = e.target;
+    const isFull = heart.innerText === FULL_HEART;
+    mimicServerCall()
+      .then(() => {
+        if (isFull) {
+          heart.innerText = EMPTY_HEART;
+        } else {
+          heart.innerText = FULL_HEART;
+          heart.classList.add('activated-heart');
+        }
+      })
+      .catch(() => {
+        const modal = document.querySelector('#modal');
+        if (modal.classList.contains('hidden')) {
+          modal.classList.remove('hidden');
+          setTimeout(() => {
+            modal.classList.add('hidden');
+          }, 3000);
+        }
+      });
   });
 }
